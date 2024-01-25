2a1516a791391e6d82774f5cdcaa25540edf92f8
Kairosh06 <karasajkajnar@gmail.com>
2024-01-26 01:55:57 +0600

    Data Types

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+x = 5
+print(type(x))
+
+int #ex1
+
+x = "Hello World"
+print(type(x))
+
+str #ex2
+
+x = 20.5
+print(type(x))
+
+float #ex3
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+list #ex4
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+tuple #ex5
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+dict #ex6
+
+x = True
+print(type(x))
+
+bool #ex7
