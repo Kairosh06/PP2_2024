1826ec17efcd48e7ca8f2f622e1b943f931d9537
Kairosh06 <karasajkajnar@gmail.com>
2024-01-26 01:06:55 +0600

    variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+carname= "Volvo" #ex1
+x = 50 #ex2
+
+x = 5
+y = 10
+print(x + y) #ex3
+
+x = 5
+y = 10
+z = x + y
+print(z) #ex4
+
+x, y, z = "Orange", "Banana", "Cherry" #ex5
+
+x = y = z = "Orange" #ex6
+
+def myfunc():
+    global x
+    x = "fantastic" #ex7
