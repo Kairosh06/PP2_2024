43719667b2cf5418832945ef04293233e8d11776
Kairosh06 <karasajkajnar@gmail.com>
2024-01-26 02:27:24 +0600

    Strings

---
+++ strings.py
@@ -0,0 +1,24 @@
+x = "Hello World"
+print(len(x)) #ex1
+
+txt = "Hello World"
+x = txt[0] #ex2
+
+txt = "Hello World"
+x = txt[2:5] #ex3
+
+txt = " Hello World "
+x = txt.strip() #ex4
+
+txt = "Hello World"
+txt = txt.upper() #ex5
+
+txt = "Hello World"
+txt = txt.lower() #ex6
+
+txt = "Hello World"
+txt = txt.replace("H", "J") #ex7
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age)) #ex8
